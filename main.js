effc38d83ca3bc02cf675f1a22deb28a5c07e23c
Parth097 <parth_uk2010@yahoo.co.uk>
2021-06-26 23:46:25 +0530

    Inital steps for the javascript file

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-//
+const display1El = document.querySelector('.display-1'); 
+const display2El = document.querySelector('.display-2'); 
+const tempResultEl = document.querySelector('.tempResult'); 
+const numbersEl = document.querySelectorAll('.number');
+const operationEl  = document.querySelectorAll('.operation');
+const equalEl = document.querySelector('.equal');
+const clearEl = document.querySelector('.all-clear');
+const clearLastEl = document.querySelectorAll('.last-entity-clear');
+
+let display1Num = '';
+let display2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
