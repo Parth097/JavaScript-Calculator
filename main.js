59b9d0250cfd5c914594c82c3f4baefb59900a21
Parth097 <parth_uk2010@yahoo.co.uk>
2021-06-27 16:33:09 +0530

    Event listener added for the numbers & operations. As well as, added the clearvariable function. This allows the screen to be cleared, and the variable stored within the other display such as tempResult.

--- main.js
+++ main.js
@@ -19,3 +19,53 @@ let display2Num = '';
 let result = null;
 let lastOperation = '';
 let haveDot = false;
+
+//add event listener so that we can press the numbers on the calculator and have them display
+
+numbersEl.forEach( number => {
+    number.addEventListener('click', (e)=> {
+        
+        // Here I am creating an exception for the decimal point.
+        // We only want 1 decimal point at a time. A simple if else statement
+        // Check to see if there is a decimal point or not. 
+
+        if(e.target.innerText === '.' && !haveDot){    
+            haveDot = true;                                 
+        }else if (e.target.innerText === '.' && haveDot){
+            return;
+        }
+        display2Num += e.target.innerText;
+        display2El.innerText = display2Num; 
+    })
+})
+
+// creating the event listener for the operaations of the calculator
+
+operationEl.forEach( operation => {
+    operation.addEventListener('click', (e)=> {
+        if(!display2Num) result;
+        haveDot = false;
+        const operationName = e.target.innerText;
+        if(display1Num && display2Num && lastOperation) {
+            mathOperation();
+        } else {
+            // using parseFloat to change the string stored within the display2Num, to a number
+            result = parseFloat(display2Num); 
+        }
+        clearVariable(operationName);
+        console.log(result);
+    })
+})
+
+// creating the clearVariable function and showing the stored variable on the correct display
+
+function clearVariable(name = '') {
+    display1Num +=  display2Num+ ' ' + name+ ' ';
+    display1El.innerText = display1Num;
+    display2El.innerText = ' ';
+    display2Num = ' ';
+    tempResultEl.innerText = result;
+}
+
+
+
